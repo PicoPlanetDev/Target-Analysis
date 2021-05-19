acb8f591d869da8f004ad5a9d535824d43f593d2
PicoPlanet Developing <picoplanetdev@gmail.com>
2021-05-19 18:18:37 -0400

    Added the install_dependencies.bat script Improved requirements.txt

---
+++ install_dependencies.bat
@@ -0,0 +1,7 @@
+pip install opencv-python --upgrade
+pip install pillow --upgrade
+pip install numpy --upgrade
+pip install argparse --upgrade
+pip install matplotlib --upgrade
+pip install ttkthemes --upgrade
+pause
