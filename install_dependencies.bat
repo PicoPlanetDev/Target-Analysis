63e94bb8d46192e710bfff20da8ead9aedec5936
PicoPlanet Developing <picoplanetdev@gmail.com>
2021-06-19 16:06:10 -0400

    Removed argparse from dependencies

--- install_dependencies.bat
+++ install_dependencies.bat
@@ -1,7 +1,8 @@
+REM Install dependencies
 pip install opencv-python --upgrade
 pip install pillow --upgrade
 pip install numpy --upgrade
-pip install argparse --upgrade
+REM pip install argparse --upgrade
 pip install matplotlib --upgrade
 pip install ttkthemes --upgrade
 pause
