61d7f6622b9a0373545c081458f838084a0dd84b
PicoPlanet Developing <picoplanetdev@gmail.com>
2021-07-29 11:22:08 -0400

    *EXPERIMENTAL* Improved install_dependencies.bat to automatically install Python 3.9.6

--- install_dependencies.bat
+++ install_dependencies.bat
@@ -1,3 +1,15 @@
+REM https://www.python.org/ftp/python/3.9.6/python-3.9.6-amd64.exe
+
+REM Downloading Python 3.9.6
+bitsadmin /transfer mydownloadjob /download /priority FOREGROUND "https://www.python.org/ftp/python/3.9.6/python-3.9.6-amd64.exe" "C:\temp\python-3.9.6-amd64.exe"
+
+REM Install Python 3.9.6
+REM Make sure to check the box to "Add Python to PATH"
+"C:\temp\python-3.9.6-amd64.exe"
+
+SET PYTHONPATH="%USERPROFILE%\AppData\LOcal\Programs\Python\Python39\python.exe"
+ECHO %PYTHONPATH%
+
 REM Install dependencies
 pip install opencv-python --upgrade
 pip install pillow --upgrade
